10692399fee03a1099901cb875ad38eae1ce634d
Zsolt Grodvalt <144676492+zsogro@users.noreply.github.com>
2026-06-04 22:11:48 +0200

    The first working DQN implementation.
    You can run the disk_test.py which loads the working checkpoints, however if you train it it might work, might not work... still looking into why the result is inconsistent.

---
+++ disk_test.py
@@ -0,0 +1,27 @@
+import time
+from tqdm import tqdm
+import pygame
+from pygame import gfxdraw
+from matplotlib import pyplot as plt
+import numpy as np
+import gymnasium as gym
+from gymnasium import spaces
+import numpy as np
+from scipy.integrate import solve_ivp
+from os import path
+import os
+from math import cos, sin
+import torch
+from torch import nn
+from copy import deepcopy
+
+from UnbalancedDisk import UnbalancedDisk_sincos, DQN, show
+
+
+max_episode_steps = 250
+env = UnbalancedDisk_sincos(dt=0.025)
+env = gym.wrappers.TimeLimit(env,max_episode_steps=max_episode_steps)
+
+Q = DQN(env, state_dim=3, action_dim=5)
+Q.load_state_dict(torch.load('Q-checkpoint-1st-working'))
+show(Q, env)
